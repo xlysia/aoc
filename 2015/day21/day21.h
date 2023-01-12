af905ef0d6c918756ac44a00119e47dfc25b247a
KiwiTae <adrien@movensee.com>
2023-01-12 11:12:11 +0100

    2015 day 21 part I and II

---
+++ 2015/day21/day21.h
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <assert.h>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <map>
+
+namespace aoc2015{
+    namespace day21{
+
+        std::string solvedayp1(std::string p);
+        std::string solvedayp2(std::string p);
+
+        void parseShop();
+        void parseBoss(std::string p);
+        void explore();
+        int p1 = 999999;
+        int p2 = -1;
+
+        void run(){
+            parseShop();
+            parseBoss("./2015/day21/day21");
+            explore();
+            std::cout << "Part 1" << std::endl;
+            std::cout << p1 << std::endl;
+
+            std::cout << "Part 2" << std::endl;
+            std::cout << p2 << std::endl;
+        }
+
+        struct Object{
+            std::string name =  "";
+            int cost = 0;
+            int damage = 0;
+            int armor = 0;
+        };
+
+        struct Player{
+            std::string name;
+            int armor;
+            int damage;
+            int health;
+        };
+        Player mine = {"me",0,0,100};
+        Player enemy = {"Boss",0,0,100};
+        std::map<std::string,std::vector<Object>> shop = {{"Weapons",{}},{"Armor",{{"none",0,0,0}}},{"Rings",{{"none1",0,0,0},{"none2",0,0,0}}}};
+
+        void parseShop(){
+            std::ifstream in("./2015/day21/shop");
+            std::cin.rdbuf(in.rdbuf());
+            std::string line;
+            std::getline(std::cin , line);
+            for(std::string type : {"Weapons","Armor","Rings"}){
+                while(std::getline(std::cin , line)){
+                    if(line.size()<1){
+                        std::getline(std::cin , line);
+                        break;
+                    }
+                    size_t pos;
+                    pos = line.find("  ");
+
+                    Object o;
+                    o.name = line.substr(0,pos);
+                    line.erase(0, pos + 1);
+                    std::stringstream ss(line);
+                    ss >> o.cost >> o.damage >> o.armor;
+                    shop[type].push_back(o);
+                }
+            }
+
+        }
+        
+        void parseBoss(std::string p){
+            std::ifstream in(p);
+            std::cin.rdbuf(in.rdbuf());
+            int x[3] = {0,0,0};
+            for(int i = 0; i < 3;i++){
+                std::string line;
+                std::getline(std::cin , line);
+                size_t pos = line.find(":");
+                line.erase(0, pos + 1);
+                std::stringstream ss(line);
+                ss >> x[i];
+            }
+            enemy.health = x[0];
+            enemy.damage = x[1];
+            enemy.armor = x[2];           
+        }
+
+        bool doIwin(){
+            int damage = std::max(1,mine.damage - enemy.armor);
+            int takingdamage = std::max(1,enemy.damage - mine.armor);
+            return damage >= takingdamage; 
+        }
+
+        void explore(){
+            for(Object w : shop["Weapons"]){
+                for(Object a : shop["Armor"]){
+                    for(Object r1 : shop["Rings"]){
+                        for(Object r2 : shop["Rings"]){
+                            mine.health = 100;
+                            mine.armor = w.armor + a.armor + r1.armor + r2.armor;
+                            mine.damage = w.damage + a.damage + r1.damage + r2.damage;
+                            int cost = w.cost + a.cost + r1.cost + r2.cost;
+                            
+                            if(doIwin()){
+                                if(cost < p1) p1 = cost;
+                            }else if(cost > p2){
+                                p2 = cost;
+                            }    
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ typedef void (*FnPtr)();
 
 std::map<std::string,FnPtr> functional_map =
 {
+  {"2015_21",aoc2015::day21::run},
   {"2015_20",aoc2015::day20::run},
   {"2015_19",aoc2015::day19::run},
   {"2015_18",aoc2015::day18::run},
