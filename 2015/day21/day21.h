4ad1436f8e4a6f87219f48c12500ddbdd36f651d
realkiwitae <taelefebvre@gmail.com>
2023-12-13 07:38:04 +0100

    aoc refactor 2015 21 to put on stack and not heap

--- 2015/day21/day21.h
+++ 2015/day21/day21.h
@@ -5,28 +5,13 @@
 #include <vector>
 #include <map>
 
+using namespace std;
+
 namespace aoc2015{
     namespace day21{
 
         std::string solvedayp1(std::string p);
         std::string solvedayp2(std::string p);
-
-        void parseShop();
-        void parseBoss(std::string p);
-        void explore();
-        int p1 = 999999;
-        int p2 = -1;
-
-        void run(){
-            parseShop();
-            parseBoss("./2015/day21/day21");
-            explore();
-            std::cout << "Part 1" << std::endl;
-            std::cout << p1 << std::endl;
-
-            std::cout << "Part 2" << std::endl;
-            std::cout << p2 << std::endl;
-        }
 
         struct Object{
             std::string name =  "";
@@ -41,15 +26,36 @@ namespace aoc2015{
             int damage;
             int health;
         };
-        Player mine = {"me",0,0,100};
-        Player enemy = {"Boss",0,0,100};
-        std::map<std::string,std::vector<Object>> shop = {{"Weapons",{}},{"Armor",{{"none",0,0,0}}},{"Rings",{{"none1",0,0,0},{"none2",0,0,0}}}};
 
-        void parseShop(){
+        void parseShop(map<std::string,std::vector<Object>>& shop);
+        void parseBoss(std::string p, Player& enemy);
+        void explore(Player& mine, Player& enemy,map<std::string,std::vector<Object>>& shop);
+        int p1 = 999999;
+        int p2 = -1;
+
+        void run(){
+            Player mine = {"me",0,0,100};
+            Player enemy = {"Boss",0,0,100};
+            std::map<std::string,std::vector<Object>> shop = {{"Weapons",{}},{"Armor",{{"none",0,0,0}}},{"Rings",{{"none1",0,0,0},{"none2",0,0,0}}}};
+            
+            parseShop(shop);
+            parseBoss("./2015/day21/day21",enemy);
+            explore(mine,enemy,shop);
+            std::cout << "Part 1" << std::endl;
+            std::cout << p1 << std::endl;
+
+            std::cout << "Part 2" << std::endl;
+            std::cout << p2 << std::endl;
+        }
+
+
+
+        void parseShop(map<std::string,std::vector<Object>>& shop){
             std::ifstream in("./2015/day21/shop");
             std::cin.rdbuf(in.rdbuf());
             std::string line;
             std::getline(std::cin , line);
+
             for(std::string type : {"Weapons","Armor","Rings"}){
                 while(std::getline(std::cin , line)){
                     if(line.size()<1){
@@ -70,7 +76,7 @@ namespace aoc2015{
 
         }
         
-        void parseBoss(std::string p){
+        void parseBoss(std::string p , Player& enemy){
             std::ifstream in(p);
             std::cin.rdbuf(in.rdbuf());
             int x[3] = {0,0,0};
@@ -87,13 +93,13 @@ namespace aoc2015{
             enemy.armor = x[2];           
         }
 
-        bool doIwin(){
+        bool doIwin(Player& mine, Player& enemy){
             int damage = std::max(1,mine.damage - enemy.armor);
             int takingdamage = std::max(1,enemy.damage - mine.armor);
             return damage >= takingdamage; 
         }
 
-        void explore(){
+        void explore(Player& mine, Player& enemy,map<std::string,std::vector<Object>>& shop){
             for(Object w : shop["Weapons"]){
                 for(Object a : shop["Armor"]){
                     for(Object r1 : shop["Rings"]){
@@ -103,7 +109,7 @@ namespace aoc2015{
                             mine.damage = w.damage + a.damage + r1.damage + r2.damage;
                             int cost = w.cost + a.cost + r1.cost + r2.cost;
                             
-                            if(doIwin()){
+                            if(doIwin(mine,enemy)){
                                 if(cost < p1) p1 = cost;
                             }else if(cost > p2){
                                 p2 = cost;
